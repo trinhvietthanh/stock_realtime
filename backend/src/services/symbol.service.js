994880865ad8a0e626dba9e0dd98b7c0a8fd2717
thanhtv <minhd@vikoisoft.com>
2025-05-04 22:23:44 +0700

    add gold api

--- backend/src/services/symbol.service.js
+++ backend/src/services/symbol.service.js
@@ -53,6 +53,20 @@ const updateSymbolPrice = async (symbolId, newPrice) => {
   return symbol;
 }
 
+const updateSymbolPriceByCode = async (code, newPrice) => {
+  const symbol = await getSymbolByCode(code);
+  if (!symbol) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Symbol not found');
+  }
+  symbol.price = newPrice;
+  await symbol.save();
+  wsService.broadcastPriceUpdate(symbol.symbol.toLowerCase(), newPrice);
+
+
+  return symbol;
+}
+
+
 const deleteSymbolById = async (symbolId) => {
   const symbol = await getSymbolById(symbolId);
   if (!symbol) {
@@ -74,6 +88,39 @@ const deleteSymbolHistory = async (symbolId) => {
   return symbol;
 }
 
+
+const fetchGoldPrice = async () => {
+  try {
+    const res = await fetch('https://www.goldapi.io/api/XAU/USD', {
+      headers: {
+        'x-access-token': "goldapi-9z5cuvsma9spa7g-io",
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) {
+      console.error(`Failed to fetch price: ${res.status}`);
+      return;
+    }
+
+    const data = await res.json();
+    cachedGoldPrice = {
+      price: data.price,
+      metal: data.metal,
+      currency: data.currency,
+      timestamp: new Date().toISOString(),
+    };
+    lastUpdated = Date.now();
+    await updateSymbolPriceByCode("xau/usd", cachedGoldPrice.price)
+    console.log(`[${cachedGoldPrice.timestamp}] Updated XAU/USD: ${data.price}`);
+  } catch (err) {
+    console.error('Error fetching gold price:', err.message);
+  }
+};
+
+// Poll every 15 seconds (or increase to avoid rate limit)
+// setInterval(fetchGoldPrice, 15000);
+// fetchGoldPrice()
 module.exports = {
   createSymbol,
   deleteSymbolById,
